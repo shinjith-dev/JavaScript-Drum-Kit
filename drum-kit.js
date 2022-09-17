d8d0e429105ea384c0943c8d6c20cd16cd7a3583
shinjith-dev <shinjithpallikkara@gmail.com>
2022-09-17 15:36:19 +0530

    improved

--- drum-kit.js
+++ drum-kit.js
@@ -1,9 +1,11 @@
+//Main audio player function
 const playKeySound = (key) => {
   const keyAudio = document.querySelector(`audio[data-key=${key}]`);
   keyAudio.currentTime = 0;
   keyAudio.play();
 };
 
+//Keyboard key recognition and highliting
 const keyPress = (e) => {
   const clickedKey = e.key.toUpperCase(),
     clickedElem = document.querySelector(`button[data-key="${clickedKey}"]`);
@@ -13,8 +15,10 @@ const keyPress = (e) => {
   }
 };
 
+//Keyboard keypress listener
 window.addEventListener("keydown", (e) => keyPress(e));
 
+//listener for removing highlighted effect
 window.addEventListener("keyup", (e) => {
   const clickedKey = e.key.toUpperCase(),
     clickedElem = document.querySelector(`button[data-key="${clickedKey}"]`);
@@ -22,6 +26,7 @@ window.addEventListener("keyup", (e) => {
     clickedElem.classList.remove("clicked");
 });
 
+//On screen button click listener
 document.querySelectorAll(".drum-cntrl").forEach((btn) => {
   btn.addEventListener("click", (e) =>
     playKeySound(e.target.dataset.key.toUpperCase())
