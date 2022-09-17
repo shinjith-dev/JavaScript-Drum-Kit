71a7376bf5bbc0abe08ea2723d0977e7d510d36f
shinjith-dev <shinjithpallikkara@gmail.com>
2022-09-17 15:26:32 +0530

    improved

---
+++ README.md
@@ -0,0 +1,14 @@
+# JavaScript Drum Kit
+
+Interactive drum kit simulation with keybindings.
+
+## For Personal Use & Customization
+
+- Clone to local and open project directory:
+
+```bash
+  git clone https://github.com/shinjith-dev/JavaScript-Drum-Kit.git
+  cd JavaScript-Drum-Kit
+```
+
+- Make your changes and open `index.html`
--- drum-kit.js
+++ drum-kit.js
@@ -1,21 +1,29 @@
-const playKeySound = (elem, key) => {
+const playKeySound = (key) => {
   const keyAudio = document.querySelector(`audio[data-key=${key}]`);
   keyAudio.currentTime = 0;
   keyAudio.play();
 };
 
-window.addEventListener("keydown", (e) => {
+const keyPress = (e) => {
   const clickedKey = e.key.toUpperCase(),
     clickedElem = document.querySelector(`button[data-key="${clickedKey}"]`);
   if (clickedElem) {
     clickedElem.classList.add("clicked");
-    playKeySound(clickedElem, clickedKey);
+    playKeySound(clickedKey);
   }
-});
+};
+
+window.addEventListener("keydown", (e) => keyPress(e));
 
 window.addEventListener("keyup", (e) => {
   const clickedKey = e.key.toUpperCase(),
     clickedElem = document.querySelector(`button[data-key="${clickedKey}"]`);
   if (clickedElem && clickedElem.classList.contains("clicked"))
     clickedElem.classList.remove("clicked");
+});
+
+document.querySelectorAll(".drum-cntrl").forEach((btn) => {
+  btn.addEventListener("click", (e) =>
+    playKeySound(e.target.dataset.key.toUpperCase())
+  );
 });
